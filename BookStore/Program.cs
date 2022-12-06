3a8b7e6174c6b8f22ecb31761e590a0cd96fe0ab
Linus <linus.thor@student.nbi-handelsakademin.se>
2022-12-06 15:09:27 +0100

    skrev hejsan

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -17,6 +17,8 @@ namespace BookStore
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //hej
+            
         }
     }
 }
