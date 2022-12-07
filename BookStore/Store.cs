896f3ca66244d1d55c81c5172f0fdc5390eb496f
Linus <linus.thor@student.nbi-handelsakademin.se>
2022-12-07 13:28:04 +0100

    La till klasserna Author, Book och Store.

---
+++ BookStore/Store.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Store
+{
+    internal class Store
+    {
+
+        //Attribut
+        /*private int id;
+        private string name;
+        private int phoneNumber;
+        private string adress;*/
+
+
+        //Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Adress { get; set; }
+
+
+        //Statisk lista
+        public static List<Store> stores = new List<Store>();
+
+        //Konstruktor
+        public Store(int id, string name, int phoneNumber, string adress)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.PhoneNumber = phoneNumber;
+            this.Adress = adress;
+
+
+            //Add THIS objekt to list
+            stores.Add(this);
+
+        }
+    }
+}
