572a71030ef7898c85bfe075b43742cd93cdbbd6
Linus <linus.thor@student.nbi-handelsakademin.se>
2022-12-08 09:44:09 +0100

    Skapat en ny klass: StoreHasBooks samt uppdaterat Author och Book klassen.

--- BookStore/Author.cs
+++ BookStore/Author.cs
@@ -7,27 +7,23 @@ public class Author
 
         //Attribut
         /*private int id;
-        private string name;
-        private string lastname;
-        private int age;*/
+        private string name;*/
+        
 
 
         //Properties
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Lastname { get; set; }
-        public int Age { get; set; }
+        
 
         //Statisk lista
         public static List<Author> authors = new List<Author>();
 
         //Konstruktor
-        public Author(int id, string name, string lastname, int age)
+        public Author(int id, string name)
         {
             this.Id = id;
             this.Name = name;
-            this.Lastname = lastname;
-            this.Age = age;
 
 
             //Add THIS objekt to list
--- BookStore/Book.cs
+++ BookStore/Book.cs
@@ -7,29 +7,30 @@ public class Book
 
         //Attribut
         /* private int id;
-         private int publishYear;
          private int price;
-         private string title;*/
+         private string title;
+         private int authorId;*/
 
 
         //Properties
         public int Id { get; set; }
         public string Title { get; set; }
-        public int PublishYear { get; set; }
         public int Price { get; set; }
+        public int AuthorId { get; set; }   
 
 
-        //Statisk lista<¨-
-        //,.'
+        //Statisk lista
+        
         public static List<Book> books = new List<Book>();
 
         //Konstruktor
-        public Book(int id, string title, int publishYear, int price)
+        public Book(int id, string title, int price, int AuthorId)
         {
             this.Id = id;
             this.Title = title;
-            this.PublishYear = publishYear;
             this.Price = price;
+            this.AuthorId = AuthorId;
+               
 
 
             //Add THIS objekt to list
